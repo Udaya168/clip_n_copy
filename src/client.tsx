577835f30f8adadc0a94a24b51f4380aa5e6853c
Udaya168 <udayakatika@gmail.com>
2026-08-13 03:06:47 +0530

    feat: configure pure static SPA build for Render static site hosting

---
+++ src/client.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { createRoot, hydrateRoot } from "react-dom/client";
+import { RouterProvider } from "@tanstack/react-router";
+import { getRouter } from "./router";
+import "./styles.css";
+
+const router = getRouter();
+const container = document.getElementById("root")!;
+
+if (container.hasChildNodes()) {
+  hydrateRoot(container, <RouterProvider router={router} />);
+} else {
+  createRoot(container).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+}
